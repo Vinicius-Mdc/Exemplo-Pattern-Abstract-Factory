d2797caad65f7c0a5b2c7a57dd912d5b7db58191
Vinicius Cruz <viniciusmdc99@gmail.com>
2024-01-15 20:35:04 -0300

    feat: configuracao inicial projeto

---
+++ src/main/java/com/example/ExemploPatternAbstractFactory/ExemploPatternAbstractFactoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.ExemploPatternAbstractFactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploPatternAbstractFactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploPatternAbstractFactoryApplication.class, args);
+	}
+
+}
